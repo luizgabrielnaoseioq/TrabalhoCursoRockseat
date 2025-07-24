2237581e2cafb754b4ca97c3f9967008ca0079af
luiz.nazario <luiz.nazario@agpr5.com>
2025-07-24 14:52:14 -0300

    fix: correct image path in index.html and clean up toggleMode function in index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function toogleMode() {
   const html = document.documentElement;
+  html.classList.toggle("light");
 
   // if(html.classList.contains('light')){
   //   html.classList.remove('light')
   // } else {
   //   html.classList.add('light')
   // }
-
-  html.classList.toggle('light')
 }
-
